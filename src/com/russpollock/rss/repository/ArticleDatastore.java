dbf05080c93d08246511d3c5b5a22b845864b2b2
Russell <ropollock@gmail.com>
2017-06-24 16:15:55 -0400

    Add ArticleDatastore documentation

--- src/com/russpollock/rss/repository/ArticleDatastore.java
+++ src/com/russpollock/rss/repository/ArticleDatastore.java
@@ -7,8 +7,40 @@ import com.russpollock.rss.repository.queries.SearchQuery;
 import java.util.List;
 
 public interface ArticleDatastore {
+    /**
+     * search
+     *
+     * Searches article datastore using a SearchQuery
+     *
+     * @param query SearchQuery
+     * @return List<ArticleHit>
+     */
     List<ArticleHit> search(SearchQuery query);
+
+    /**
+     * indexAll
+     *
+     * Adds a list of articles to the article datastore
+     *
+     * @param documents List of Articles
+     */
     void indexAll(final List<Article> documents);
+
+    /**
+     * index
+     *
+     * Adds an article to the article datastore
+     *
+     * @param article Article
+     */
     void index(final Article article);
+
+    /**
+     * delete
+     *
+     * Removes an article from the article datastore
+     *
+     * @param id String
+     */
     void delete(final String id);
 }
